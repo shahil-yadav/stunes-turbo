d13d88130dceae871967a834f27c672e848efe05
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-12-28 16:19:18 +0530

    scroll to top
    Apparently nextjs gets confused as far as I can relate, with nested layouts to scroll to top

--- apps/www/lib/constants.ts
+++ apps/www/lib/constants.ts
@@ -1,12 +1,2 @@
-import { loadEnvConfig } from "@next/env";
-import path from "node:path";
-
-const projectDir = path.dirname(
-  process.cwd().split(path.sep).slice(0, -1).join(path.sep),
-);
-
-loadEnvConfig(projectDir);
-
 export const BASE_URL = process.env.BASE_URL;
-if (!BASE_URL)
-  throw new Error(`BASE_URL is required, vercel path = ${projectDir}`);
+if (!BASE_URL) throw new Error(`BASE_URL is required, .env is not supplied`);
