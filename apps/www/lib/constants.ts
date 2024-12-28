8185157fe02237bdad4783634e8ef4be7611ff51
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-12-28 12:31:36 +0530

    Integrating .env support for vercel frontend

--- apps/www/lib/constants.ts
+++ apps/www/lib/constants.ts
@@ -1,11 +1,11 @@
-const hostname = "localhost:3000";
-const protocol = "http";
-export const URL = `${protocol}://${hostname}`;
-export const selectedImageIndex = -1;
-export const selectedAudioIndex = -1;
-export const language = "english,hindi";
-export const SECONDS_REQUIRED_TO_INSERT_IN_THE_HISTORY = 30;
-export const LIMIT_RECOMMENDATION_SONGS = 5;
+import { loadEnvConfig } from "@next/env";
+import path from "node:path";
+
+const projectDir = path.dirname(
+  process.cwd().split(path.sep).slice(0, -1).join(path.sep),
+);
+
+loadEnvConfig(projectDir);
 
 export const BASE_URL = process.env.BASE_URL;
 if (!BASE_URL)
