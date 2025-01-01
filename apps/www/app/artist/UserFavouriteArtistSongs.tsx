5c9441066933fcb5ea5d2ace21e8e0e86c0a902a
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2025-01-01 10:48:58 +0530

    updated prettier config & props passing to manage label

--- apps/www/app/artist/UserFavouriteArtistSongs.tsx
+++ apps/www/app/artist/UserFavouriteArtistSongs.tsx
@@ -1,47 +1,52 @@
-"use client";
+"use client"
 
-import { useAppSelector } from "@/lib/redux/hooks";
-import { selectHistorySongIds } from "@/lib/redux/controls-slice";
-import useSWRImmutable from "swr/immutable";
-import { LibrarySong } from "@/app/library/library-song";
-import { z } from "zod";
-import React from "react";
+import { useAppSelector } from "@/lib/redux/hooks"
+import { selectHistorySongIds } from "@/lib/redux/controls-slice"
+import useSWRImmutable from "swr/immutable"
+import { LibrarySong } from "@/app/library/library-song"
+import { z } from "zod"
+import React from "react"
 
-export function RecommendedArtistSongsBaseOnClientHistory({
-  artistId,
-}: {
-  artistId: string;
-}) {
-  const songIds = useAppSelector(selectHistorySongIds);
+export function RecommendedArtistSongsBaseOnClientHistory({ artistId }: { artistId: string }) {
+  const songIds = useAppSelector(selectHistorySongIds)
+  const [isDisplayLabel, setIsDisplayLabel] = React.useState<boolean>(false)
   return (
     <div className="my-5">
-      <p className="text-3xl">Yours top &apos;s favourites listened</p>
+      {isDisplayLabel && <p className="text-3xl">Yours top &apos;s favourites listened</p>}
       {songIds.map((songId) => (
-        <Recommendations key={songId} songId={songId} artistId={artistId} />
+        <Recommendations
+          key={songId}
+          songId={songId}
+          artistId={artistId}
+          setIsDisplayLabel={setIsDisplayLabel}
+        />
       ))}
     </div>
-  );
+  )
 }
 
 function Recommendations({
   songId,
-  artistId,
+  artistId
+  setIsDisplayLabel
 }: {
-  songId: string;
-  artistId: string;
+  songId: string
+  artistId: string
+  setIsDisplayLabel: React.Dispatch<React.SetStateAction<boolean>>
 }) {
-  const swr = useSWRImmutable(`/api/songs?songId=${songId}`);
-  const [isDisplay, setIsDisplay] = React.useState<boolean>(false);
+  const swr = useSWRImmutable(`/api/songs?songId=${songId}`)
+  const [isDisplay, setIsDisplay] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    if (swr.isLoading || swr.error) return;
-    const data = swr.data ?? [];
-    const artistIds = data?.at(0).artists?.all?.map((artist) => artist?.id);
-    const parsedResults = z.string().array().safeParse(artistIds);
+    if (swr.isLoading || swr.error) return
+    const data = swr.data ?? []
+    const artistIds = data?.at(0).artists?.all?.map((artist) => artist?.id)
+    const parsedResults = z.string().array().safeParse(artistIds)
     if (parsedResults.success && parsedResults.data.includes(artistId)) {
-      setIsDisplay(true);
+      setIsDisplay(true)
+      setIsDisplayLabel(true)
     }
-  }, [swr.data, swr.isLoading, swr.error, artistId]);
+  }, [swr.data, swr.isLoading, swr.error, artistId])
 
-  return isDisplay && <LibrarySong songId={songId} />;
+  return isDisplay && <LibrarySong songId={songId} />
 }
--- apps/www/lib/constants.ts
+++ apps/www/lib/constants.ts
@@ -1,8 +1,8 @@
-const BASE_URL = process.env.BASE_URL;
-const BASE_URL_2 = process.env.BASE_URL_2;
+const BASE_URL = process.env.BASE_URL
+const BASE_URL_2 = process.env.BASE_URL_2
 
-if (!BASE_URL || !BASE_URL_2)
-  throw new Error(`BASE_URL is required, .env is not supplied`);
+if (!BASE_URL || !BASE_URL_2) {
+  throw new Error(`BASE_URL is required, .env is not supplied`)
+}
 
-export  {BASE_URL, BASE_URL_2};
-
+export { BASE_URL, BASE_URL_2 }
